901133977954a780f766e18a06f1f5d8cf7a1520
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2022-02-04 22:48:52 -0800

    connection to frontend

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,23 @@
+import { Server } from "socket.io";
 export default function main() {
+	const io = new Server({
+		cors: {
+			origin: "*",
+		}
+	});
+	io.on('connection', (socket) => {
+		socket.send({
+			type: 'message',
+			data: 'Hello World'
+		})
+	});
+	io.on('message', ()=>{
+		return {
+			message: 'Hello World'
+		}
+	})
+	
+	io.listen(420);
 	console.log("Hello World!");
 }
 
